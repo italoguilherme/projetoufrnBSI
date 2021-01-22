48dcbaaedc05448b6361aafc24294c810145e677
Italo <italog09@gmail.com>
2021-01-22 15:33:05 -0300

    projeto UFRN BSI

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-///////////////////////////////////////////////////////////////////////////////
-///             Universidade Federal do Rio Grande do Norte                 ///
-///                 Centro de Ensino Superior do Seridó                     ///
-///               Departamento de Computação e Tecnologia                   ///
-///                  Disciplina DCT1106 -- Programação                      ///
-///                  Projeto Controle de fluxo de veiculos                  ///
-///                Developed by  @flgorgonio - Jan, 2021                    ///
-///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+///Universidade Federal do Rio Grande do Norte                    ///                
+///Centro de Ensino Superior do Seridó                           ///         
+///Bacharelado em Sistema de Informação                         ///            
+///Disciplina DCT1106 -- Programação                           ///         
+///Projeto Controle de fluxo para entrada e saida de veiculos ///         
+///Developed by  Ítalo G and Lucas V - Jan, 2021             /// 
+///////////////////////////////////////////////////////////////
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,26 +20,14 @@ int main(void) {
 
 
 void menuSobre(void) {
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-    printf("///               Departamento de Computação e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
-    printf("///                Developed by  @flgorgonio - Jan, 2021                    ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = Sistema de Gestão Escolar = = = = =                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///  Programa exemplo utilizado na disciplina DCT1106 - Programação, para   ///\n");
-    printf("///  fins didáticos de ilustração. O programa contém os principais módulos  ///\n");
-    printf("///  e funcionalidades que serão exigidos na avaliação dos projetos a serem ///\n");
-    printf("///  desenvolvidos pelos alunos ao longo da disciplina. Serve como um guia  ///\n");
-    printf("///  de consulta e referência para o desenvolvidos dos demais projetos.     ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
+    printf("         ////////////////////////////////////////////////////////////////////////////\n");
+    printf("        ///                 ///                                                  ///\n");
+    printf("       ///      *BSI*      ///                                                  ///\n");
+    printf("      ///                 ///                                                  ///\n");
+    printf("     ///////////////////////                                                  ///\n");
+    printf("    ///                                                                      ///\n");
+    printf("   ///                                                                      ///\n");
+    printf("  ///                                                                      ///\n");
+    printf(" ///                                                                      ///\n");
+    printf("////////////////////////////////////////////////////////////////////////////\n");
 }
